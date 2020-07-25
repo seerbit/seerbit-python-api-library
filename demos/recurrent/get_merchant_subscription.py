e0423d6ae8bfd4b7fc570c038ca15bd4370d4dc1
centric123 <abubakar.eliakwu@yahoo.com>
2020-07-25 14:17:36 +0100

    update

--- demos/recurrent/get_merchant_subscription.py
+++ demos/recurrent/get_merchant_subscription.py
@@ -40,27 +40,18 @@ def authenticate() -> str:
     return auth_service.get_token()
 
 
-def update_subscription(token_str: str):
-    """ Initiate Recurrent Subscription """
-    print("================== start update subscription ==================")
-    recurring_payload = {
-        "amount": "200",
-        "currency": "NGN",
-        "country": "NG",
-        "mobileNumber": "08033456500",
-        "billingId": "PUBK_PjQ5d1578650322483",
-        "publicKey": client.public_key,
-        "status": "INACTIVE"
-    }
+def get_merchant_subscription(token_str: str):
+    """ Get Merchant Subscription """
+    print("================== start get merchant subscription ==================")
     recurring_service = RecurringService(client, token_str)
-    json_response = recurring_service.update_subscription(recurring_payload)
-    print("================== stop update subscription ==================")
+    json_response = recurring_service.get_merchant_subscriptions(client.public_key)
+    print("================== stop get merchant subscription ==================")
     return json_response
 
 
 token = authenticate()
 
 if token:
-    print("update subscription response: " + str(update_subscription(token)))
+    print("get customer subscription response: " + str(get_merchant_subscription(token)))
 else:
     print("authentication failure")
